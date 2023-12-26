6171a796e601cae9934d37bc87d63162aa22df43
ashwin15082002 <ashwin15082002@gmail.com>
2023-12-26 17:31:57 +0530

    ongoing gantt view

--- gantt_view/__manifest__.py
+++ gantt_view/__manifest__.py
@@ -6,15 +6,16 @@
     'depends': ['base', 'project', 'web'],
     'author': "Ashwin",
     'category': 'category',
-    'description': """  """,
+    'description': """ Custom gantt view for project module. """,
     'installable': True,
     'application': False,
     'license': 'LGPL-3',
-    # data files always loaded at installation
+
     'data': ['views/project_gantt_view.xml',],
     'assets': {
         'web.assets_backend': [
             'gantt_view/static/src/xml/gantt_view.xml',
+            'gantt_view/static/src/js/gantt_view.js',
         ],
     }
 }
